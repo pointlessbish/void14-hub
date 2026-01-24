2ec9f061f3f513c19d9e9cb42b8f2841dbf2bf69
pointlessbish <pointlessbish@gmail.com>
2026-01-24 07:47:32 +0100

    fix: switched back for troubleshooting purposes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server', // This makes the site dynamic
-  adapter: cloudflare() // This tells it to run on Cloudflare's edge
+  adapter: cloudflare()
 });
